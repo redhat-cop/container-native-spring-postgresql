6c9bb14ccade11ce19a48e32000ded91bdc686f8
sshamsi <sshamsi@localhost.localdomain>
2018-01-29 13:46:16 -0500

    updated application id generators

--- spring/product-inventory/src/main/java/com/redhat/productinventory/entities/Inventory.java
+++ spring/product-inventory/src/main/java/com/redhat/productinventory/entities/Inventory.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Inventory {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long invId;
 	
 	private Integer qty;
