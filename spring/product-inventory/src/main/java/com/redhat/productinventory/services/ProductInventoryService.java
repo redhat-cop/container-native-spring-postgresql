34720ccda822d4812a3a81e4cdd2b08f39b1b0f3
Eric Murphy <murphye@gmail.com>
2018-02-01 15:32:53 -0500

    Add content type due to 400 bad request

--- spring/product-inventory/src/main/java/com/redhat/productinventory/services/ProductInventoryService.java
+++ spring/product-inventory/src/main/java/com/redhat/productinventory/services/ProductInventoryService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -38,6 +39,7 @@ public class ProductInventoryService implements ProductInventory {
 	public List<InventoryDTO> showInventoryList(String token) throws JsonProcessingException, IOException {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", token);
+		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 		HttpEntity<?> requestheaders = new HttpEntity<>(headers);
 		List<Inventory> invList = inventoryRepository.findAll();
 		List<InventoryDTO> inventory = new ArrayList<>();
